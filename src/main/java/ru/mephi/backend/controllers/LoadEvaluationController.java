11db1879a0d5c17f441de4d6105afe125a938ca5
yevgn <zhenya041010@gmail.com>
2024-10-02 23:07:26 +0300

    Добавлена документация для свагера

--- src/main/java/ru/mephi/backend/controllers/LoadEvaluationController.java
+++ src/main/java/ru/mephi/backend/controllers/LoadEvaluationController.java
@@ -1,14 +1,17 @@
 package ru.mephi.backend.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import ru.mephi.backend.dto.*;
 import ru.mephi.backend.service.LoadEvaluationService;
@@ -20,10 +23,29 @@ import java.net.URISyntaxException;
 @RequiredArgsConstructor
 @RequestMapping("/api/load_evaluation")
 @Slf4j
+@CrossOrigin
+@Tag(
+        name = "Контроллер для оценки запаса/дефицита пропускной способности в час-пик транспортных объектов " +
+                "при возникновении дополнительного спроса"
+)
 public class LoadEvaluationController {
 
     private final LoadEvaluationService loadEvaluationService;
 
+    @Operation(
+            summary = "Получение оценки спроса/дефицита пропускной способности объекта в час-пик" +
+                    " в случае если пользователь ввел площадь застройки вручную",
+            description = "Оценка дается в виде пар ключ-знание. Ключ - транспортный объект. " +
+                    "Значение - результат (разность между максимальной пропускной способностью объекта и пассажиропотоком" +
+                    " с учетом дополнительного спроса)"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/area")
     public ResponseEntity<LoadResponse> getLoadEvaluationByArea(@RequestBody LoadRequestWithArea loadRequest) {
 
@@ -35,6 +57,22 @@ public class LoadEvaluationController {
         }
     }
 
+    @Operation(
+            summary = "Получение оценки спроса/дефицита пропускной способности объекта в час-пик в случае " +
+                    "если пользователь отметил точки территории застройки",
+            description = "В данном случае на сервере вычисляется центр координат и суммарная поэтажная площадь. " +
+                    "Оценка спроса/дефицита пропускной способности объекта дается в виде пар ключ-знание." +
+                    " Ключ - транспортный объект. " +
+                    "Значение - результат (разность между максимальной пропускной способностью объекта" +
+                    " и пассажиропотоком с учетом дополнительного спроса)"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/polygon")
     public ResponseEntity<LoadResponse> getLoadEvaluationByPolygon(
             @RequestBody LoadRequestWithPolygon loadRequest){
--- src/main/java/ru/mephi/backend/controllers/UtilityController.java
+++ src/main/java/ru/mephi/backend/controllers/UtilityController.java
@@ -1,5 +1,11 @@
 package ru.mephi.backend.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.mephi.backend.dto.AreaRequest;
@@ -8,38 +14,117 @@ import ru.mephi.backend.dto.LoadAdd;
 import ru.mephi.backend.dto.PolygonRequest;
 import ru.mephi.backend.service.UtilityService;
 
+
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin
+@Tag(
+        name = "Контроллер для вычислительных операций над объектом застройки"
+)
 @RequestMapping("/api/utility")
 public class UtilityController {
 
     private final UtilityService utilityService;
 
+    @Operation(
+            summary = "Вычисление площади застройки",
+            description = "Вычисление площади застройки по координатам"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/square")
     public double calculateSquare(@RequestBody PolygonRequest polygonRequest) {
         return utilityService.calculateSquare(polygonRequest);
     }
 
+    @Operation(
+            summary = "Вычисление кол-ва населения, которое будет использовать УДС и станции метро в час-пик " +
+                    "в результате застройки территории, если пользователь сам отметил точки территории застройки " +
+                    "на карте",
+            description = "От общего числа дополнительного населения берем 10% в случае жилой площади, 35% - в случае" +
+                    " офисной застройки"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/population/polygon")
     public int calculatePopulation(@RequestBody PolygonRequest polygonRequest) {
         return utilityService.calculatePopulationFromPolygonRequest(polygonRequest);
     }
 
+    @Operation(
+            summary = "Вычисление кол-ва населения, которое будет использовать УДС и станции метро в час-пик " +
+                    "в результате застройки территории, если пользователь вручную ввел площадь застройки",
+            description = "От общего числа дополнительного населения берем 10% в случае жилой площади, 35% - в случае" +
+                    " офисной застройки"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/population/area")
     public int calculatePopulationArea(@RequestBody AreaRequest areaRequest) {
         return utilityService.calculatePopulationFromAreaRequest(areaRequest);
     }
 
+    @Operation(
+            summary = "Вычисление суммарного дополнительного спроса на УДС и станции метро в час-пик в " +
+                    "результате застройки, в случае если пользователь отметил точки территории застройки на карте",
+            description = "70% всего спроса идет на станции метро, 30% - на авто (УДС)"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/load/polygon")
     public LoadAdd calculateLoad(@RequestBody PolygonRequest polygonRequest) {
         return utilityService.calculateLoadFromPolygon(polygonRequest);
     }
 
+    @Operation(
+            summary = "Вычисление суммарного дополнительного спроса на УДС и станции метро в час-пик в " +
+                    "результате застройки, в случае если пользователь вручную ввел площадь застройки",
+            description = "70% всего спроса идет на станции метро, 30% - на авто (УДС)"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/load/area")
     public LoadAdd calculateLoadArea(@RequestBody AreaRequest areaRequest) {
         return utilityService.calculateLoadFromArea(areaRequest);
     }
 
+
+    @Operation(
+            summary = "Вычисление центра координат застройки",
+            description = "Вычисление центра координат застройки по введенным точкам"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "404", description = "Не найдено",
+                    content = @Content(mediaType = "application/json", examples = { @ExampleObject(
+                            value = "{\"message\": \"Данной страницы не существует\", \"debugMessage\":\"" +
+                                    "Данной страницы не существует\"}") })),
+            @ApiResponse(responseCode = "200", description = "ОК")
+    })
     @PostMapping("/centroid")
     public Coordinate calculateCentroid(@RequestBody PolygonRequest polygonRequest) {
         return utilityService.calculateCentroid(polygonRequest);
